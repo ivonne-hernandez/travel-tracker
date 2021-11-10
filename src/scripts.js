9c14ed9df881fff6a7af85b4f6f21a40132fdd4a
Ivonne Hernandez <ivonne702@gmail.com>
2021-11-10 15:58:33 -0700

    Import fetch call GET functions from apiCalls.js and process each Promise

--- src/scripts.js
+++ src/scripts.js
@@ -7,5 +7,26 @@ import './css/base.scss';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
+import {
+  fetchAllTravelersData,
+  fetchSingleTravelerData, 
+  fetchAllTrips, 
+  fetchAllDestinations
+} from './apiCalls';
 
-console.log('This is the JavaScript entry file - your code begins here.');
+const fetchAll = () => {
+  const allTravelersDataPromise = fetchAllTravelersData();
+  const singleTravelerDataPromise = fetchSingleTravelerData(1);//need to pass in the userId
+  const allTripsDataPromise = fetchAllTrips();
+  const allDestinationsDataPromise = fetchAllDestinations();
+  
+  Promise.all([allTravelersDataPromise, singleTravelerDataPromise, allTripsDataPromise, allDestinationsDataPromise])
+  .then(data => {
+    console.log(`allTravelersDataPromise:`, data[0]);
+    console.log(`singleTravelerDataPromise:`, data[1]);
+    console.log(`allTrips:`, data[2]);
+    console.log(`allDestinationsDataPromise:`, data[3]);
+  })
+}
+
+fetchAll();
