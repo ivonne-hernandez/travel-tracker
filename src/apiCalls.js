6bef412a0570ccc8356c0c88213245dfbf525600
Ivonne Hernandez <ivonne702@gmail.com>
2021-11-10 15:57:35 -0700

    Add fetch call functions for GET requests

---
+++ src/apiCalls.js
@@ -0,0 +1,19 @@
+export let fetchAllTravelersData = () => {
+  return fetch("http://localhost:3001/api/v1/travelers")
+    .then(response => response.json())
+}
+
+export let fetchSingleTravelerData = (id) => {
+  return fetch(`http://localhost:3001/api/v1/travelers/${id}`)
+    .then(response => response.json())
+}
+//where<id> will be a number of a traveler’s id
+export let fetchAllTrips = () => {
+  return fetch("http://localhost:3001/api/v1/trips")
+  .then(response => response.json())
+}
+
+export let fetchAllDestinations = () => {
+  return fetch("http://localhost:3001/api/v1/destinations")
+    .then(response => response.json())
+}
