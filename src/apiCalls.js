2c1df33d4f769577ef85efa8b925012e250479f5
Ivonne Hernandez <ivonne702@gmail.com>
2021-11-11 10:53:57 -0700

    Add comment for login id

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -7,7 +7,7 @@ export let fetchSingleTravelerData = (id) => {
   return fetch(`http://localhost:3001/api/v1/travelers/${id}`)
     .then(response => response.json())
 }
-//where<id> will be a number of a traveler’s id
+//where<id> will be a number of a traveler’s id from login
 export let fetchAllTrips = () => {
   return fetch("http://localhost:3001/api/v1/trips")
   .then(response => response.json())
