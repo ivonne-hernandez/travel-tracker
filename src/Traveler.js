035550f2c19a0da2ef138314f7a3db5fa9da7a9b
Ivonne Hernandez <ivonne702@gmail.com>
2021-11-11 18:00:45 -0700

    Add constructor parameters, properties, and shell methods

--- src/Traveler.js
+++ src/Traveler.js
@@ -1,5 +1,25 @@
 class Traveler {
-  constructor(data) {
+  constructor(data, tripsForTraveler) {
+    this.userId = data.id;
+    this.name = data.name;
+    this.travelerType = data.travelerType;
+    this.trips = tripsForTraveler;
+  }
+
+  getPastTrips() {
+    
+  }
+  
+  getPresentTrips() {
+
+  }
+
+  getUpcomingTrips() {
+
+  }
+
+  getPendingTrips() {
+
   }
 }
-
+export default Traveler;
