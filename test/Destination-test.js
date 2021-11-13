407cf614d4d1b6e6d13d3a47b7be55d527a499ad
Ivonne Hernandez <ivonne702@gmail.com>
2021-11-13 00:53:26 -0700

    Add tests for the constructor properties

--- test/Destination-test.js
+++ test/Destination-test.js
@@ -26,5 +26,28 @@ describe('Destination', () => {
     expect(destination).to.be.an.instanceOf(Destination);
   });
 
+  it('should have an id', function() {
+    expect(destination.id).to.equal(1);
+  });
   
+  it('should have a destination', function() {
+    expect(destination.destination).to.equal(destinationData.destination);
+  });
+
+  it('should have an estimatedLodgingCostPerDay', function() {
+    expect(destination.estimatedLodgingCostPerDay).to.equal(destinationData.estimatedLodgingCostPerDay);
+  });
+
+  it('should have an estimatedFlightCostPerPerson', function() {
+    expect(destination.estimatedFlightCostPerPerson).to.equal(destinationData.estimatedFlightCostPerPerson);
+  });
+
+  it('should have an image', function() {
+    expect(destination.image).to.equal(destinationData.image);
+  });  
+
+  it('should have an alt', function() {
+    expect(destination.alt).to.equal(destinationData.alt);
+  });  
+
 });
