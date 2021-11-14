afd12fedd406e70a1ac6b4d872d40143366bd8f9
Ivonne Hernandez <ivonne702@gmail.com>
2021-11-14 12:12:52 -0700

    Add displayNewTripForm, addEventListenersForForm, areValidUserInputFields, getEstimatedTripCost and displayEstimatedTripCost functions to display a cost estimate

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,3 +1,6 @@
+import Destination from "./Destination";
+import Trip from "./Trip";
+
 let domUpdates = {
   mainContainer: document.querySelector('.main-container'),
 
@@ -6,33 +9,105 @@ let domUpdates = {
     welcomeTraveler.innerText = `Welcome ${traveler.name}`
   },
 
-  displayNewTripForm() {
+
+  displayNewTripForm(allDestinations) {
+    const listOfAllDestinationNames = allDestinations
+      .map(destination => {
+        const instantiatedDestination = new Destination(destination);
+        return `<option value="${instantiatedDestination.id}">${instantiatedDestination.destination}</option>`;
+      })
     this.mainContainer.innerHTML = `
       <h2>Add New Trip</h2>
       <form id="newTripInputForm" class="new-trip-form">
         <div>
           <label for="date">Date</label>
-          <input id="date" type="date">
+          <input id="dateInput" type="date">
         </div>
         <div>
           <label for="duration">Duration</label>
-          <input id="duration" type="number">
+          <input id="durationInput" type="number">
         </div>
         <div>
           <label for="number-of-travelers">Number of Travelers</label>
-          <input id="number-of-travelers" type="number">
+          <input id="numberOfTravelersInput" type="number">
         </div>
         <div>
           <label for="destination">Destination</label>
-          <input id="destination" type="text">
+          <select name="destination" id="destinationNameInput">
+          ${listOfAllDestinationNames}
+          </select>
         </div>
         <div>
-          <label for="estimated-cost">Estimated Cost</label>
-          <p id="estimated-cost" type="number">
+          <label id="estimatedCost" for="estimated-cost">Estimated Cost</label>
+        </div>
+        <div>
+          <button class="submit-trip-request-button" id="submitTripRequestButton" disabled>Submit Trip Request</button>
         </div>
       </form>
     `;
+    
+    this.addEventListenersForForm(allDestinations);
   }, 
+
+  addEventListenersForForm(allDestinations) {
+    const newTripInputForm = document.querySelector('#newTripInputForm');
+    newTripInputForm.addEventListener('input', function() {
+      if (domUpdates.areValidUserInputFields()) {
+        domUpdates.getEstimatedTripCost(allDestinations);
+      }
+    });
+
+    const submitTripRequestButton = document.querySelector('#submitTripRequestButton');
+    submitTripRequestButton.addEventListener('click', function(event) {
+      event.preventDefault();
+      domUpdates.postNewTripRequest();
+    });
+  },
+
+  areValidUserInputFields() {
+    const dateInput = document.querySelector('#dateInput');
+    const durationInput = document.querySelector('#durationInput');
+    const numberOfTravelersInput = document.querySelector('#numberOfTravelersInput');
+    const destinationNameInput = document.querySelector('#destinationNameInput');
+    const submitTripRequestButton = document.querySelector('#submitTripRequestButton');
+
+    const inputsAreNotEmpty = dateInput.value && durationInput.value && numberOfTravelersInput.value && destinationNameInput.value;
+    const duration = Number(durationInput.value);
+    const numberOfTravelers = Number(numberOfTravelersInput.value);
+    if (inputsAreNotEmpty && duration > 0 && numberOfTravelers > 0) {
+      submitTripRequestButton.disabled = false;
+      return true;
+    } else {
+      submitTripRequestButton.disabled = true;
+      return false;
+    }
+  },
+
+  getEstimatedTripCost(allDestinations) {
+    const duration = Number(document.querySelector('#durationInput').value);
+    const numberOfTravelers = Number(document.querySelector('#numberOfTravelersInput').value);
+    const destinationId = Number(document.querySelector('#destinationNameInput').value);
+    const matchingTripDestination = allDestinations.find(destination => destinationId === destination.id);
+    const tripForEstimationPurposesOnly = {
+      "id": 1,
+      "userID": 44,
+      "destinationID": destinationId,
+      "travelers": numberOfTravelers,
+      "date": "2022/09/16",
+      "duration": duration,
+      "status": "pending",
+      "suggestedActivities": []
+    }
+    const trip = new Trip(tripForEstimationPurposesOnly, new Destination(matchingTripDestination));
+    const estimatedTripTotal = trip.calculateCost();
+
+    this.displayEstimatedTripCost(estimatedTripTotal);
+  },
+
+  displayEstimatedTripCost(estimatedTripTotal) {
+    const estimatedCost = document.querySelector('#estimatedCost');
+    estimatedCost.innerHTML = `<p>Estimated Cost: $${estimatedTripTotal.toLocaleString()}</p>`
+  },
 
   displayTrips(trips, type) {
     this.mainContainer.innerHTML = `<h2>${type} Trips:</h2>`;
@@ -71,7 +146,23 @@ let domUpdates = {
     `;
   }
 
+//  I should be able to make a trip request:
+// I will select a date, duration, number of travelers and choose from a list of destinations
+  //Need to look into dropdown list that user can select from for destinations
+// After making these selections, I should see an estimated cost (with a 10% travel agent fee) for the trip.
+  //Need to change the Estimated Cost to a disabled button, once all of the input fields are valid
+  //the user can click on the "Get Estimated Cost" button to get the cost (event listener on this button that calculates and displays the cost on the DOM upon click)
+  //We also need a submit trip request button that is disabled at the same time as the "est cost button"
+// Once I submit the trip request, it will show on my dashboard as “pending” so that the travel agency can approve or deny it.
+  //once the "submit trip request" button is pressed (event listener on this button) there should be a POST request
+  //and then a fetch call that will "refresh" the data s.t. the trip is now in the "Pending Trips" section
   
 }
 
+
+
+
+
+
 export default domUpdates;
+
